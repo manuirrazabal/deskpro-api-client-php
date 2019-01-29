c9ffe400f6eb78bf2f7a91c30ef3c1b8dc795777
Julien Ducro <julien.ducro@deskpro.com>
2019-01-29 14:39:39 +0100

    Remove .git folders from release file

--- build.sh
+++ build.sh
@@ -9,4 +9,5 @@ cp composer.json build/deskpro/
 cp build-extra/include.php build/deskpro/
 cp build-extra/README.md build/deskpro/
 cd build
+( find . -type d -name ".git" && find . -name ".gitignore" && find . -name ".gitmodules" ) | xargs rm -rf
 zip -r deskpro.zip deskpro
